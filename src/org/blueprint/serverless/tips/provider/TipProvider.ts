48488369ed1a21d1f885cce2268e0ed598b88e58
sarthakmakhija <sarthak.makhija@gmail.com>
2019-11-28 01:30:47 +0530

    Sarthak | Refactors provideTips method in TipProvider class

--- src/org/blueprint/serverless/tips/provider/TipProvider.ts
+++ src/org/blueprint/serverless/tips/provider/TipProvider.ts
@@ -9,9 +9,17 @@ export class TipProvider {
     }
 
     provideTips(): Tip[] {
-        let allTips = this.tipStore.allTips();
+        return this.nTips();
+    }
+
+    private nTips() {
+        let allTips = this.allTips();
         if (allTips.length === 0)
             return [];
         return allTips;
+    }
+
+    private allTips() {
+        return this.tipStore.allTips();
     }
 }
