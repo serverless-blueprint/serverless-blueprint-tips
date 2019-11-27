53ba706a957b00cc8c879693346727787950e01d
sarthakmakhija <sarthak.makhija@gmail.com>
2019-11-28 01:22:43 +0530

    Sarthak | Adds a concept of TipStore with a capability to return all tips

--- test/org/blueprint/serverless/tips/TipProvider.spec.ts
+++ test/org/blueprint/serverless/tips/TipProvider.spec.ts
@@ -1,14 +1,71 @@
 import {expect} from 'chai';
+import * as sinon from 'sinon';
 
 class TipProvider {
-    provideTips() {
-        return [];
+    private tipStore: TipStore;
+
+    constructor() {
+        this.tipStore = TipStore.instance();
+    }
+
+    provideTips(): Tip[] {
+        let allTips = this.tipStore.allTips();
+        if (allTips.length === 0)
+            return [];
+        return allTips;
+    }
+}
+
+class Tip {
+    constructor(public readonly id: number) {
+    }
+}
+
+class TipStore {
+
+    private constructor() {
+    }
+
+    private static ref: TipStore;
+
+    static instance() {
+        if (TipStore.ref == null)
+            TipStore.ref = new TipStore();
+
+        return TipStore.ref;
+    }
+
+    allTips(): Tip[] {
+        return null;
     }
 }
 
 describe("Tip Provider", () => {
+
+    let tipStore;
+    let mock;
+
+    beforeEach(() => {
+        tipStore = TipStore.instance();
+        mock = sinon.mock(tipStore);
+    });
+
+    afterEach(() => {
+        sinon.restore();
+    });
+
+
     it("should generate zero tips", () => {
-       let tips = new TipProvider().provideTips();
-       expect(tips).to.be.empty;
+        mock.expects("allTips").returns([]);
+
+        let tips = new TipProvider().provideTips();
+        expect(tips).to.be.empty;
+    });
+
+    it("should generate a single tip with a tip-id", () => {
+        mock.expects("allTips").returns([new Tip(1)]);
+
+        let tips = new TipProvider().provideTips();
+        expect(tips[0].id).to.equal(1);
     });
 });
