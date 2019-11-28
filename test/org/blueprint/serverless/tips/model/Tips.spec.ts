f788c9d04206c1fcecc6c29e27ef2517134b4d71
sarthakmakhija <sarthak.makhija@gmail.com>
2019-11-28 12:39:38 +0530

    Sarthak | Adds unit tests for count of Tips

--- test/org/blueprint/serverless/tips/model/Tips.spec.ts
+++ test/org/blueprint/serverless/tips/model/Tips.spec.ts
@@ -13,4 +13,14 @@ describe("Tips", () => {
         let tips = new Tips([new Tip(1, "", "")]);
         expect(tips.isEmpty()).to.be.false;
     });
+
+    it("should return 0 as the tip count", () => {
+        let tips = new Tips(null);
+        expect(tips.count()).to.equal(0);
+    });
+
+    it("should return 1 as the tip count", () => {
+        let tips = new Tips([new Tip(1, "", "")]);
+        expect(tips.count()).to.equal(1);
+    });
 });
